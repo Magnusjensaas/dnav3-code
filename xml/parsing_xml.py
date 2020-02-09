c796b4b3c6f43362b0c3d8a170c1ef211f26df1d
magnusjensaas <magnusjensaas@gmail.com>
2020-02-09 17:51:43 +0100

    Python script for parsing XML and prettyfing a XML string

--- xml/parsing_xml.py
+++ xml/parsing_xml.py
@@ -8,3 +8,12 @@ for elem in pool:
     print(elem.firstChild.data)
 
 
+xmlString = "<server><pool>http</pool><description>My virtual server test</description><name>http-virtual</name><mask>255.255.255.255</mask><profiles><name>http</name><kind>ltm:virtual:profile</kind><name>tcp</name><kind>ltm:virtual:profile</kind></profiles><ipProtocol>tcp</ipProtocol><sourceAddressTranslation><type>automap</type></sourceAddressTranslation><kind>tm:ltm:virtual:virtualstate</kind><destination>1.1.1.3</destination></server>"
+dom = minidom.parseString(xmlString)
+xml = dom.toprettyxml()
+print(xml)
+
+
+
+
+
